6418e5febc527321ac63bb105a8b5144fc612dbc
leah.digiteer@gmail.com <leah.digiteer@gmail.com>
2025-08-19 18:11:49 +0800

    chore: wrap app with CartProvider and add Cart screen to navigation

--- App.tsx
+++ App.tsx
@@ -4,12 +4,15 @@ import { createStackNavigator } from '@react-navigation/stack';
 import ProductList from './screens/ProductList';
 import ProductDetails from './screens/ProductDetails';
 import { RootStackParamList } from './types/navigation';
+import { CartProvider } from "./context/CartContext";
+import CartScreen from './screens/CartScreen';
 
 
 const Stack = createStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
+    <CartProvider>
     <NavigationContainer>
        <Stack.Navigator initialRouteName="ProductList">
         <Stack.Screen
@@ -22,7 +25,13 @@ export default function App() {
           component={ProductDetails}
           options={{ title: "Product Details" }}
         />
+         <Stack.Screen
+            name="Cart"
+            component={CartScreen}
+            options={{ title: "My Cart" }}
+          />
       </Stack.Navigator>
     </NavigationContainer>
+    </CartProvider>
   );
 }
