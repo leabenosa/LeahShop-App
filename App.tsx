810138372a4cfcd34729a8b434bd9bdfefaa46dd
leah.digiteer@gmail.com <leah.digiteer@gmail.com>
2025-08-19 18:40:32 +0800

    feat: setup Stack Navigator with ProductList, ProductDetails, and Cart screens with header cart icon

--- App.tsx
+++ App.tsx
@@ -6,39 +6,42 @@ import ProductDetails from './screens/ProductDetails';
 import { RootStackParamList } from './types/navigation';
 import { CartProvider } from "./context/CartContext";
 import CartScreen from './screens/CartScreen';
-import { CartIcon } from "./components/CartIcon"; 
-
+import { CartIcon } from "./components/CartIcon";
 
 const Stack = createStackNavigator<RootStackParamList>();
+
+// Move headerRight outside App component
+const ProductListHeaderRight = (navigation: any) => <CartIcon navigation={navigation} />;
+const ProductDetailsHeaderRight = (navigation: any) => <CartIcon navigation={navigation} />;
 
 export default function App() {
   return (
     <CartProvider>
-    <NavigationContainer>
-       <Stack.Navigator initialRouteName="ProductList">
-        <Stack.Screen
-          name="ProductList"
-          component={ProductList}
-          options={({ navigation }) => ({
-          title: "Leah's Shop",
-          headerRight: () => <CartIcon navigation={navigation} />,
-          })}
-        />
-        <Stack.Screen
-          name="ProductDetails"
-          component={ProductDetails}
-          options={({ navigation }) => ({
-             title: "Product Details",
-            headerRight:() => <CartIcon navigation={navigation} />,
-           })}
-        />
-         <Stack.Screen
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="ProductList">
+          <Stack.Screen
+            name="ProductList"
+            component={ProductList}
+            options={({ navigation }) => ({
+              title: "Leah's Shop",
+              headerRight: () => ProductListHeaderRight(navigation),
+            })}
+          />
+          <Stack.Screen
+            name="ProductDetails"
+            component={ProductDetails}
+            options={({ navigation }) => ({
+              title: "Product Details",
+              headerRight: () => ProductDetailsHeaderRight(navigation),
+            })}
+          />
+          <Stack.Screen
             name="Cart"
             component={CartScreen}
             options={{ title: "My Cart" }}
           />
-      </Stack.Navigator>
-    </NavigationContainer>
+        </Stack.Navigator>
+      </NavigationContainer>
     </CartProvider>
   );
 }
